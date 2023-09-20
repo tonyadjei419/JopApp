f4c6541ca0abc84a414a65584c5393203488ab00
tonyadjei419 <adjeitony653@gmail.com>
2023-09-20 00:20:34 +0000

    first commit

---
+++ hook/useFetch.js
@@ -0,0 +1,50 @@
+import { useState,useEffect} from 'react'
+import axios from 'axios';
+
+
+const useFetch = (endpoint,query) =>  {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+ 
+
+const options = {
+  method: 'GET',
+  url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+  headers: {
+    'X-RapidAPI-Key': '886f5f22b0msheaa8d3bc9e4db99p1170f7jsnd44b4c831b2c',
+    'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+  },
+ params: { ...query },
+  
+};
+const fetchData = async () =>  {
+  setIsLoading(true);
+
+  try{
+    const response = await axios.request
+    (options);
+
+    setData(response.data.data);
+    setIsLoading(false)
+  } catch (error) {
+    setError(error);
+    alert('There is an error')
+  } finally {
+    setIsLoading(false);
+  }
+}
+   useEffect(() => {
+    fetchData();
+   }, [])
+
+const refetch = () => {
+  setIsLoading(true);
+  fetchData();
+}
+
+return { data,isLoading,error,refetch};
+
+}
+
+export default useFetch 
